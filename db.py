db395fa120a861d2952d33fb94fa61a5df700dfb
00008420-collab <00008420@uca.edu.sv>
2025-11-23 09:49:59 -0600

    Update db.py

--- db.py
+++ db.py
@@ -4,11 +4,11 @@ from sqlalchemy import create_engine, MetaData
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.automap import automap_base
 
-DB_USER = os.getenv("DB_USER")
-DB_PASS = os.getenv("DB_PASS")
-DB_HOST = os.getenv("DB_HOST", "127.0.0.1")
+DB_USER = os.getenv("DB_USER", "uvfz1bhg53yj6hed")
+DB_PASS = os.getenv("DB_PASS", "F2wwhaaKkEUbS4annYMP")
+DB_HOST = os.getenv("DB_HOST", "bewuh9yx8e9gctyl08lp-mysql.services.clever-cloud.com")
 DB_PORT = os.getenv("DB_PORT", "3306")
-DB_NAME = os.getenv("DB_NAME")
+DB_NAME = os.getenv("DB_NAME", "bewuh9yx8e9gctyl08lp")
 
 CONN = f"mysql+pymysql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}?charset=utf8mb4"
 
