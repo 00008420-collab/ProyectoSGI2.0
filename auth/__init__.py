7d484a8ff0a5078ef9232abb2d59756fb3180f45
00008420-collab <00008420@uca.edu.sv>
2025-11-24 02:03:00 -0600

    Delete auth directory

--- auth/__init__.py
+++
@@ -1,3 +0,0 @@
-# auth/__init__.py
-from .login import authenticate_db, authenticate_static, show_login_streamlit, logout
-__all__ = ["authenticate_db", "authenticate_static", "show_login_streamlit", "logout"]
